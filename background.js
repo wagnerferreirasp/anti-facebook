43676edea424d28985a99e0ac22235eb35f6e34f
Wagner Ferreira Alves <wagnerferreirasp@hotmail.com>
2016-03-23 21:53:31 -0300

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.tabs.getSelected(null, function(tab){
         chrome.tabs.executeScript(tab.id,{code:"document.getElementById('mainContainer').innerHTML = ''"});
-        chrome.tabs.executeScript(tab.id,{code:"document.getElementsByClassName('_4kny')[0].innerHTML = ''"});
 });
-alert("Agora voce pode trabalhar tranquilo!");
+alert("No more news feed!");
